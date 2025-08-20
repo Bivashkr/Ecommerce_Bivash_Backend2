44a13b300ebc43e93a798fa2e33b5b6e5506f8ad
Bivash Kumar <bivashkumar828@gmail.com>
2025-08-20 18:33:49 +0530

    aaa

--- src/main/java/com/zosh/modal/Order.java
+++ src/main/java/com/zosh/modal/Order.java
@@ -1,32 +1,24 @@
 package com.zosh.modal;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.zosh.user.domain.OrderStatus;
 
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @Getter
 @Setter
@@ -34,39 +26,38 @@ import java.util.Set;
 @Table(name = "orders")
 public class Order {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String orderId;
-  
-    @ManyToOne
-    private User user;
+	private String orderId;
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<OrderItem> orderItems = new ArrayList<>();
+	@ManyToOne
+	private User user;
 
-    private LocalDate orderDate = LocalDate.now();
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<OrderItem> orderItems = new ArrayList<>();
 
-    private LocalDate deliveryDate;
+	private LocalDate orderDate = LocalDate.now();
 
-    @OneToOne
-    private Address shippingAddress;
+	private LocalDate deliveryDate;
 
-    @Embedded
-    private PaymentDetails paymentDetails=new PaymentDetails();
+	@OneToOne
+	private Address shippingAddress;
 
-    private double totalPrice;
-    
-    private Integer totalDiscountedPrice;
-    
-    private Integer discount;
+	@Embedded
+	private PaymentDetails paymentDetails = new PaymentDetails();
 
-    private OrderStatus orderStatus;
-    
-    private int totalItem;
-    
-    private LocalDateTime createdAt;
+	private double totalPrice;
 
+	private Integer totalDiscountedPrice;
+
+	private Integer discount;
+
+	private OrderStatus orderStatus;
+
+	private int totalItem;
+
+	private LocalDateTime createdAt;
 
 }
