24de49ac23cadc3548dc69267a55920abdd813ac
Bivash Kumar <bivashkumar828@gmail.com>
2025-08-20 17:54:44 +0530

    gg

--- src/main/java/com/zosh/modal/Category.java
+++ src/main/java/com/zosh/modal/Category.java
@@ -1,5 +1,12 @@
 package com.zosh.modal;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -7,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -16,64 +24,21 @@ import lombok.Data;
 @Table(name = "categories")
 @Data
 public class Category {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
+    @NotNull
+    @Size(max = 50)
+    private String name;
+    
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "parent_category_id")
+    private Category parentCategory;
 
-	@NotNull
-	@Size(max = 50)
-	private String name;
+    private int level;
+    
 
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "parent_category_id")
-	private Category parentCategory;
-
-	private int level;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Category getParentCategory() {
-		return parentCategory;
-	}
-
-	public void setParentCategory(Category parentCategory) {
-		this.parentCategory = parentCategory;
-	}
-
-	public int getLevel() {
-		return level;
-	}
-
-	public void setLevel(int level) {
-		this.level = level;
-	}
-
-	public Category(Long id, @NotNull @Size(max = 50) String name, Category parentCategory, int level) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.parentCategory = parentCategory;
-		this.level = level;
-	}
-
-	public Category() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
 
 }
--- src/main/java/com/zosh/modal/Size.java
+++ src/main/java/com/zosh/modal/Size.java
@@ -1,7 +1,11 @@
 package com.zosh.modal;
 
+
 import jakarta.persistence.Embeddable;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Embeddable
 @Data
@@ -9,32 +13,5 @@ public class Size {
 
 	private String name;
 	private int quantity;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-
-	public Size(String name, int quantity) {
-		super();
-		this.name = name;
-		this.quantity = quantity;
-	}
-
-	public Size() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
 
 }
