0cc63699cac2b8f487bbddbd47d8235bf8eff98a
Bivash Kumar <bivashkumar828@gmail.com>
2025-08-20 17:29:20 +0530

    fgkafj

---
+++ src/main/java/com/zosh/modal/Size.java
@@ -0,0 +1,40 @@
+package com.zosh.modal;
+
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+@Embeddable
+@Data
+public class Size {
+
+	private String name;
+	private int quantity;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Size(String name, int quantity) {
+		super();
+		this.name = name;
+		this.quantity = quantity;
+	}
+
+	public Size() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
